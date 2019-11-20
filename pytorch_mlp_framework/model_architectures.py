d67bfc7cfb65942f38570b0f0760c56b29f2981e
Wenye <2591194414@qq.com>
2019-11-20 20:24:55 +0000

    Update model_architectures.py

--- pytorch_mlp_framework/model_architectures.py
+++ pytorch_mlp_framework/model_architectures.py
@@ -369,7 +369,7 @@ class ConvolutionalProcessingBlock_bn_res(nn.Module):
         x = torch.zeros(self.input_shape)
         out = x
 	
-	self.layer_dict['bn_0'] =nn.BatchNorm2d(out.shape[1])
+        self.layer_dict['bn_0'] =nn.BatchNorm2d(out.shape[1])
         out = self.layer_dict['bn_0'].forward(out)
         out = F.leaky_relu(out)
         self.layer_dict['conv_0'] = nn.Conv2d(in_channels=out.shape[1], out_channels=self.num_filters, bias=self.bias,
