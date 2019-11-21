778cfaed60392d5d551523e9876a8749a93433ba
Wenye <2591194414@qq.com>
2019-11-21 14:26:09 +0000

    Update train_evaluate_image_classification_system.py

--- pytorch_mlp_framework/train_evaluate_image_classification_system.py
+++ pytorch_mlp_framework/train_evaluate_image_classification_system.py
@@ -17,7 +17,7 @@ transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.RandomRotation(60),
-        transforms.Grayscale(num_output_channels=1)
+        transforms.Grayscale(1)
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
