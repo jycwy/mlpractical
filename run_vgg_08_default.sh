21dfd08314fe68e805de69746f884c49c76e3b64
Wenye <2591194414@qq.com>
2019-11-21 01:02:12 +0000

    Update run_vgg_08_default.sh

--- run_vgg_08_default.sh
+++ run_vgg_08_default.sh
@@ -1,5 +1,3 @@
 <<<<<<< HEAD
 python pytorch_mlp_framework/train_evaluate_image_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --num_stages 3 --num_blocks_per_stage 0 --experiment_name VGG_08_experiment --use_gpu True --num_classes 100 --block_type 'conv_dropout_block' --continue_from_epoch -1
-=======
-python pytorch_mlp_framework/train_evaluate_image_classification_system.py --batch_size 100 --seed 0 --num_filters 32 --num_stages 3 --num_blocks_per_stage 0 --experiment_name VGG_08_experiment --use_gpu True --num_classes 100 --block_type 'conv_block' --continue_from_epoch -1
->>>>>>> 420aa5f752399c4236096ce7a9ac8e81f5726530
+
